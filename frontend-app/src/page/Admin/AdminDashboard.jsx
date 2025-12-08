658d77dd23aba961bc93d49d4168dbd124e91e98
Jhalak yadav <yadavjhalak3211@gmail.com>
2025-12-08 13:20:42 +0530

    staff button added

--- frontend-app/src/page/Admin/AdminDashboard.jsx
+++ frontend-app/src/page/Admin/AdminDashboard.jsx
@@ -145,6 +145,12 @@ console.log('yes')
   >
     Create User
   </button>
+  <button 
+  onClick={() => navigate("/dashboard/admin/staff-performance")}
+  className="w-full bg-white/10 hover:bg-white/20 transition rounded-lg px-4 py-2 text-left"
+>
+  Staff
+</button>
 </div>
 
       </aside>
